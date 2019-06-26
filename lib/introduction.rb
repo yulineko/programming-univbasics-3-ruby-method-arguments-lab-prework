5597be75319fb907ef1886ebd45f35091335dd20
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-06-26 22:35:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,4 +6,5 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}.js.\n"
 end
 
-def 
+def introduction_with_language_optional(name, language = "Ruby")
+  puts 
